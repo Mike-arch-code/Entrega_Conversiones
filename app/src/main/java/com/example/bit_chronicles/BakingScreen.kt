6040c70be6b8db5edc87b1ec4b84952d2eda7a50
Mike <maicol.andre@2004gmail.com>
2025-05-14 20:28:14 -0500

    Create project

---
+++ app/src/main/java/com/example/bit_chronicles/BakingScreen.kt
@@ -0,0 +1,107 @@
+package com.example.bit_chronicles
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+
+
+
+@Composable
+fun BakingScreen(
+    bakingViewModel: BakingViewModel = viewModel()
+) {
+    val placeholderPrompt = stringResource(R.string.prompt_placeholder)
+    val placeholderResult = stringResource(R.string.results_placeholder)
+    var prompt by rememberSaveable { mutableStateOf(placeholderPrompt) }
+    var result by rememberSaveable { mutableStateOf(placeholderResult) }
+    val uiState by bakingViewModel.uiState.collectAsState()
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+
+
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            TextField(
+                value = prompt,
+                label = { Text(stringResource(R.string.label_prompt)) },
+                onValueChange = { prompt = it },
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(end = 8.dp)
+                    .align(Alignment.CenterVertically)
+            )
+
+            Button(
+                onClick = {
+                    bakingViewModel.sendPrompt(prompt)
+                },
+                enabled = prompt.isNotEmpty() && uiState !is UiState.Loading,
+                modifier = Modifier.align(Alignment.CenterVertically)
+            ) {
+                Text(text = stringResource(R.string.action_go))
+            }
+        }
+
+        if (uiState is UiState.Loading) {
+            CircularProgressIndicator(
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(vertical = 16.dp)
+            )
+        } else {
+            var textColor = MaterialTheme.colorScheme.onSurface
+            if (uiState is UiState.Error) {
+                textColor = MaterialTheme.colorScheme.error
+                result = (uiState as UiState.Error).errorMessage
+            } else if (uiState is UiState.Success) {
+                textColor = MaterialTheme.colorScheme.onSurface
+                result = (uiState as UiState.Success).outputText
+            }
+            val scrollState = rememberScrollState()
+            Text(
+                text = result,
+                textAlign = TextAlign.Start,
+                color = textColor,
+                modifier = Modifier
+                    .padding(top = 16.dp)
+                    .fillMaxWidth()
+                    .weight(1f)
+                    .verticalScroll(scrollState)
+            )
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun BakingScreenPreview() {
+     BakingScreen()
+}
---
+++ app/src/main/java/com/example/bit_chronicles/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.bit_chronicles
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.bit_chronicles.ui.theme.Bit_ChroniclesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Bit_ChroniclesTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+                    BakingScreen()
+                }
+            }
+        }
+    }
+}
